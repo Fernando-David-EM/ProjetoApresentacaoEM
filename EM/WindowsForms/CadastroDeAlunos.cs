9efd59055e426baa3f59ee1e11fa11b1d57b760e
Fernando-David-EM <fernando.david@escolarmanager.com.br>
2020-08-10 13:54:22 -0300

    first commit

---
+++ EM/WindowsForms/CadastroDeAlunos.cs
@@ -0,0 +1,43 @@
+﻿using ProjetoApresentacaoEM.EM.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoApresentacaoEM
+{
+    public partial class CadastroDeAlunos : Form
+    {
+        public CadastroDeAlunos()
+        {
+            InitializeComponent();
+            comboBoxSexo.Items.Add(EnumeradorSexo.Masculino);
+            comboBoxSexo.Items.Add(EnumeradorSexo.Feminino);
+        }
+
+        internal void InsereNoCampoMatricula(int matriculaAluno)
+        {
+            textBoxMatricula.Text = matriculaAluno.ToString();
+        }
+
+        internal void InsereNoCampoNome(string nomeDoAluno)
+        {
+            textBoxNome.Text = nomeDoAluno;
+        }
+
+        internal void SelecionaSexo(EnumeradorSexo masculino)
+        {
+            comboBoxSexo.SelectedItem = masculino;
+        }
+
+        internal void InsereNoCampoDataDeNascimento(DateTime data)
+        {
+            
+        }
+    }
+}
---
+++ EM/WindowsForms/Tela.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ProjetoApresentacaoEM
+{
+    partial class CadastroDeAlunos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxInfo = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxMatricula = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxSexo = new System.Windows.Forms.ComboBox();
+            this.groupBoxInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxInfo
+            // 
+            this.groupBoxInfo.Controls.Add(this.comboBoxSexo);
+            this.groupBoxInfo.Controls.Add(this.label3);
+            this.groupBoxInfo.Controls.Add(this.textBoxNome);
+            this.groupBoxInfo.Controls.Add(this.label2);
+            this.groupBoxInfo.Controls.Add(this.textBoxMatricula);
+            this.groupBoxInfo.Controls.Add(this.label1);
+            this.groupBoxInfo.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxInfo.Name = "groupBoxInfo";
+            this.groupBoxInfo.Size = new System.Drawing.Size(590, 171);
+            this.groupBoxInfo.TabIndex = 0;
+            this.groupBoxInfo.TabStop = false;
+            this.groupBoxInfo.Text = "Novo aluno";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matrícula";
+            // 
+            // textBoxMatricula
+            // 
+            this.textBoxMatricula.Location = new System.Drawing.Point(10, 37);
+            this.textBoxMatricula.Name = "textBoxMatricula";
+            this.textBoxMatricula.Size = new System.Drawing.Size(121, 20);
+            this.textBoxMatricula.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(148, 37);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(430, 20);
+            this.textBoxNome.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sexo";
+            // 
+            // comboBoxSexo
+            // 
+            this.comboBoxSexo.FormattingEnabled = true;
+            this.comboBoxSexo.Location = new System.Drawing.Point(10, 81);
+            this.comboBoxSexo.Name = "comboBoxSexo";
+            this.comboBoxSexo.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSexo.TabIndex = 5;
+            // 
+            // CadastroDeAlunos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 450);
+            this.Controls.Add(this.groupBoxInfo);
+            this.Name = "CadastroDeAlunos";
+            this.Text = "Cadastro de Alunos";
+            this.groupBoxInfo.ResumeLayout(false);
+            this.groupBoxInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxInfo;
+        private System.Windows.Forms.TextBox textBoxMatricula;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxSexo;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
